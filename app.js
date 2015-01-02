92f738a38b790c0cc562737dc0811d4f7b63805d
hynding <shynding@mac-lp-shynding.office.leads360.com>
2015-01-02 13:05:55 -0800

    replaced with MEANJS

--- app.js
+++
@@ -1,9 +0,0 @@
-var restify = require("restify");
-	mongoose = require("mongoose"),
-	oyl = require("./oyl.js"),
-	db = mongoose.connection;
-
-db.on( "error", console.error.bind(console, "con err"));
-db.once( "open", oyl.up );
-
-mongoose.connect("mongodb://localhost/oyl");
