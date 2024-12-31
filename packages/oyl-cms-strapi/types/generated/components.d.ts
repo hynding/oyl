8ca7c30f7b8ae019dd526753d79b4305bb3620af
Steve Hynding <steve.hynding@gmail.com>
2024-12-30 20:45:58 -0800

    updates

--- packages/oyl-cms-strapi/types/generated/components.d.ts
+++ packages/oyl-cms-strapi/types/generated/components.d.ts
@@ -1,81 +1,81 @@
-import type { Struct, Schema } from '@strapi/strapi';
-
-export interface ResumeSkills extends Struct.ComponentSchema {
-  collectionName: 'components_resume_skills';
-  info: {
-    displayName: 'Skill';
-    icon: 'cloud';
-    description: '';
-  };
-  attributes: {
-    name: Schema.Attribute.String;
-    experience: Schema.Attribute.Enumeration<
-      ['Beginner', 'Intermediate', 'Advanced', 'Expert']
-    > &
-      Schema.Attribute.DefaultTo<'Intermediate'>;
-  };
-}
-
-export interface ResumeSkillType extends Struct.ComponentSchema {
-  collectionName: 'components_resume_skill_types';
-  info: {
-    displayName: 'Skill Field';
-    icon: 'bulletList';
-    description: '';
-  };
-  attributes: {
-    name: Schema.Attribute.String;
-    skills: Schema.Attribute.Component<'resume.skills', true>;
-  };
-}
-
-export interface ResumeExperience extends Struct.ComponentSchema {
-  collectionName: 'components_resume_experiences';
-  info: {
-    displayName: 'Experience';
-    icon: 'layer';
-  };
-  attributes: {
-    name: Schema.Attribute.String & Schema.Attribute.Required;
-    location: Schema.Attribute.String;
-    dateStart: Schema.Attribute.Date & Schema.Attribute.Required;
-    dateEnd: Schema.Attribute.Date;
-    description: Schema.Attribute.RichText;
-    url: Schema.Attribute.String;
-  };
-}
+import type { Schema, Struct } from "@strapi/strapi"
 
 export interface DietDailyConsumables extends Struct.ComponentSchema {
-  collectionName: 'components_diet_daily_consumables';
+  collectionName: "components_diet_daily_consumables"
   info: {
-    displayName: 'Daily Consumables';
-    icon: 'restaurant';
-    description: '';
-  };
+    description: ""
+    displayName: "Daily Consumables"
+    icon: "restaurant"
+  }
   attributes: {
     consumable: Schema.Attribute.Relation<
-      'oneToOne',
-      'api::consumable.consumable'
-    >;
+      "oneToOne",
+      "api::consumable.consumable"
+    >
     servings: Schema.Attribute.Decimal &
       Schema.Attribute.Required &
       Schema.Attribute.SetMinMax<
         {
-          min: 0.01;
+          min: 0.01
         },
         number
-      >;
-    time: Schema.Attribute.Time & Schema.Attribute.Required;
-  };
+      >
+    time: Schema.Attribute.Time & Schema.Attribute.Required
+  }
 }
 
-declare module '@strapi/strapi' {
+export interface ResumeExperience extends Struct.ComponentSchema {
+  collectionName: "components_resume_experiences"
+  info: {
+    displayName: "Experience"
+    icon: "layer"
+  }
+  attributes: {
+    dateEnd: Schema.Attribute.Date
+    dateStart: Schema.Attribute.Date & Schema.Attribute.Required
+    description: Schema.Attribute.RichText
+    location: Schema.Attribute.String
+    name: Schema.Attribute.String & Schema.Attribute.Required
+    url: Schema.Attribute.String
+  }
+}
+
+export interface ResumeSkillType extends Struct.ComponentSchema {
+  collectionName: "components_resume_skill_types"
+  info: {
+    description: ""
+    displayName: "Skill Field"
+    icon: "bulletList"
+  }
+  attributes: {
+    name: Schema.Attribute.String
+    skills: Schema.Attribute.Component<"resume.skills", true>
+  }
+}
+
+export interface ResumeSkills extends Struct.ComponentSchema {
+  collectionName: "components_resume_skills"
+  info: {
+    description: ""
+    displayName: "Skill"
+    icon: "cloud"
+  }
+  attributes: {
+    experience: Schema.Attribute.Enumeration<
+      ["Beginner", "Intermediate", "Advanced", "Expert"]
+    > &
+      Schema.Attribute.DefaultTo<"Intermediate">
+    name: Schema.Attribute.String
+  }
+}
+
+declare module "@strapi/strapi" {
   export module Public {
     export interface ComponentSchemas {
-      'resume.skills': ResumeSkills;
-      'resume.skill-type': ResumeSkillType;
-      'resume.experience': ResumeExperience;
-      'diet.daily-consumables': DietDailyConsumables;
+      "diet.daily-consumables": DietDailyConsumables
+      "resume.experience": ResumeExperience
+      "resume.skill-type": ResumeSkillType
+      "resume.skills": ResumeSkills
     }
   }
 }
