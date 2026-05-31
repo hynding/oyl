663fa95adac26616adb295c3243a63c3cd773186
Steve Hynding <steve.hynding@gmail.com>
2026-05-31 14:52:13 -0700

    feat(daily-new): wire page to data providers, add goals column

--- packages/react-oyl/modules/user/daily-new/UserDailyPage.tsx
+++ packages/react-oyl/modules/user/daily-new/UserDailyPage.tsx
@@ -1,21 +1,21 @@
+// packages/react-oyl/modules/user/daily-new/UserDailyPage.tsx
 import UserDailyHeader from './UserDailyHeader'
-import UserDailyActivities from './activities'
-import UserDailyProvider from './UserDailyProvider'
+import { UserDailyActivities } from './activities'
+import { UserDailyGoals } from './goals'
+import UserDailyDataProviders from './UserDailyDataProviders'
 
 export default function UserDailyPage() {
-
   return (
-    <UserDailyProvider>
+    <UserDailyDataProviders>
       <div className="min-h-screen w-full bg-gray-50 dark:bg-gray-900 py-8">
-        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
           <UserDailyHeader />
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <UserDailyActivities />
-            {/* <UserDailyGoalsSection />
-            <UserDailyNutritionSection /> */}
+            <UserDailyGoals />
           </div>
         </div>
       </div>
-    </UserDailyProvider>
+    </UserDailyDataProviders>
   )
 }
