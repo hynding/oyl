0bc6da717e9b3024ed07911d5983076cdfb7068a
Steve Hynding <steve.hynding@gmail.com>
2026-05-30 19:29:37 -0700

    feat(user/goal-milestone): add provider with CRUD and toggle/reorder

---
+++ packages/react-oyl/modules/user/goal-milestone/index.ts
@@ -0,0 +1,4 @@
+// packages/react-oyl/modules/user/goal-milestone/index.ts
+export { default as UserGoalMilestoneProvider } from './UserGoalMilestoneProvider'
+export { useUserGoalMilestoneContext } from './user-goal-milestone-context'
+export type { UserGoalMilestoneContextValue } from './user-goal-milestone-context'
