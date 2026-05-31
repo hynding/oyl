faa7bc819127c787118e413e019a824e57569b2c
Steve Hynding <steve.hynding@gmail.com>
2026-05-31 15:00:37 -0700

    chore(daily-new): drop obsolete sections/ stubs, PROPOSAL.md, deprecated settings

--- packages/react-oyl/modules/user/daily-new/sections/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as UserDailyActivitiesSection } from './UserDailyActivitiesSection'
-export { default as UserDailyGoalsSection } from './UserDailyGoalsSection'
-export { default as UserDailyNutritionSection } from './UserDailyNutritionSection'
